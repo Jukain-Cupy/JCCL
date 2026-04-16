c77d83e9be1c9943b5d3ecdecb2f2be4f99c8361
cupy <>
2026-04-16 21:28:27 +0100

    implemented ceaser cipher functions

---
+++ ceaser.h
@@ -0,0 +1,49 @@
+#ifndef CEASER_H
+#define CEASER_H
+
+// Encrypt a character by a certain shift
+static inline char JCCL_ceaserEncryptChar(char c, int shift){
+    char encryptedC;
+
+    // Support uppercase
+    if(c >= 'A' && c <= 'Z'){
+        encryptedC = ((c - 'A' + shift) % 26) + 'A';
+    
+    // Support lowercase
+    }else if(c >= 'a' && c <= 'z'){
+        encryptedC = ((c - 'a' + shift) % 26) + 'a';
+    
+    // Keep non-letters the same
+    }else{
+        encryptedC = c;
+    }
+
+    return encryptedC;
+}
+
+// Decrypt a character that was encrypted by a certain shift
+static inline char JCCL_ceaserDecryptChar(char c, int shift){
+    int decryptShift = 26 - (shift % 26);
+
+    return JCCL_ceaserEncryptChar(c, decryptShift);
+}
+
+// Encrypt an entire string by a certain shift, storing result in outStr
+static inline char* JCCL_ceaserEncryptStr(char* str, int strLen, int shift, char* outStr){
+    for(int i=0; i<strLen; i++){
+        outStr[i] = JCCL_ceaserEncryptChar(str[i], shift);
+    }
+
+    outStr[strLen] = '\0';
+}
+
+// Decrypt an entire string that was encrypted by a certain shift, storing result in outStr
+static inline void JCCL_ceaserDecryptStr(char* str, int strLen, int shift, char* outStr){
+    for(int i=0; i<strLen; i++){
+        outStr[i] = JCCL_ceaserDecryptChar(str[i], shift);
+    }
+
+    outStr[strLen] = '\0';
+}
+
+#endif
